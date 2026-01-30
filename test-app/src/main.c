512eb2b73c40e646cc7b9cb21b71f5df43edf8bf
Zahra Suhail <zahrasuhail15@gmail.com>
2026-01-30 18:51:00 -0500

    Adding custom configuration such that one can choose when to use custom code

--- test-app/src/main.c
+++ test-app/src/main.c
@@ -1,14 +1,24 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#ifdef CONFIG_MYFUNCTION
 #include "myfunction.h"
+#endif
 
 int main(void) {
+
   int a = 3;
   int b = 4;
   int result = sum(a, b);
   while (1) {
+
+#ifdef CONFIG_MYFUNCTION
     printk("The sum of %d and %d is %d\n", a, b, result);
+#else
+    printk("My Function is disabled. Enable CONFIG_MYFUNCTION to use it.\n"); 
+    return 0;
+#endif
     k_msleep(1000);
   }
+  
 }
