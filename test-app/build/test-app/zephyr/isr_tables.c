589f0f95ccee34d338dabca57c31a1dc9d78d52a
Zahra Suhail <zahrasuhail15@gmail.com>
2026-01-30 18:17:03 -0500

    Application updated: a simple addition program

--- test-app/build/test-app/zephyr/isr_tables.c
+++ test-app/build/test-app/zephyr/isr_tables.c
@@ -58,13 +58,13 @@ const uintptr_t __irq_vector_table _irq_vector_table[48] = {
 	((uintptr_t)&_isr_wrapper),
 };
 const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
-	{(const void *)0x4809, (ISR)0x473d}, /* 0 */
+	{(const void *)0x481d, (ISR)0x4751}, /* 0 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 1 */
-	{(const void *)0x52d4, (ISR)0x45b9}, /* 2 */
+	{(const void *)0x52e8, (ISR)0x45cd}, /* 2 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 3 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 4 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 5 */
-	{(const void *)0x20000034, (ISR)0x453b}, /* 6 */
+	{(const void *)0x20000034, (ISR)0x454f}, /* 6 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 7 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 8 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 9 */
@@ -75,7 +75,7 @@ const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 14 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 15 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 16 */
-	{(const void *)0x0, (ISR)0x1d7d}, /* 17 */
+	{(const void *)0x0, (ISR)0x1d8d}, /* 17 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 18 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 19 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 20 */
--- test-app/src/main.c
+++ test-app/src/main.c
@@ -1,9 +1,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#include "myfunction.h"
+
 int main(void) {
+  int a = 3;
+  int b = 4;
+  int result = sum(a, b);
   while (1) {
-    printk("Hello, Zephyr!\n");
+    printk("The sum of %d and %d is %d\n", a, b, result);
     k_msleep(1000);
   }
 }
